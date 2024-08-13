731d22f8da6bfaed774df20ed064df99a809dda5
ajbtech23 <joshuabanjo.tech@gmail.com>
2024-08-13 20:19:07 +0100

    Closing Chapter 3

--- chapter_3.rb
+++ chapter_3.rb
@@ -11,3 +11,15 @@ puts composer + ' was "da bomb", in his day.'
 
 composer = 'Beethoven'
 puts 'But I prefer ' + composer + ', personally.'
+
+var1 = 8 
+var2 = var1
+
+puts var1
+puts var2
+
+puts ''
+
+var1 = 'eight'
+puts var1
+puts var2
