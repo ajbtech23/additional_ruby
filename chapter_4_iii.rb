bea96a020a74c126f48e6923750546d9966fb91b
ajbtech23 <joshuabanjo.tech@gmail.com>
2024-08-13 20:45:50 +0100

    Tighter and tighter

---
+++ chapter_4_iii.rb
@@ -0,0 +1,7 @@
+puts 'Aye Aye! And who ye be Sailor?'
+first_name = gets.chomp 
+puts 'Captain ' + first_name + ' say ye? And your last name you young Scallywag?'
+last_name = gets.chomp
+puts 'Well shiver me timbers! Captain ' + first_name + ' of house ' + last_name + '.'
+puts 'Never did I think I\'d see the day...'
+
