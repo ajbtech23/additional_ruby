8ce37ed913db656020140721aae4e2df577f097d
ajbtech23 <joshuabanjo.tech@gmail.com>
2024-08-13 20:15:29 +0100

    Focusing on working with variables

--- chapter_3.rb
+++ chapter_3.rb
@@ -2,3 +2,7 @@ myString = '...you can say that again...'
 puts myString
 puts myString
 
+name = 'Patricia Rosanna Jessica Mildred Oppenheimer'
+puts 'My name is ' + name + '.'
+puts 'Wow! ' + name + ' is a really long name!'
+
