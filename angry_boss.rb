bc052df6ba97e65a4393700dacc419d1939c5223
ajbtech23 <joshuabanjo.tech@gmail.com>
2024-08-14 15:44:06 +0100

    Completed to the desired standard and moving onto the next exercise

--- angry_boss.rb
+++ angry_boss.rb
@@ -4,3 +4,9 @@ puts 'What was that?! You said you want ' + employee_request.upcase + '? Did I h
 employee_request = gets.chomp
 puts 'Can\'t be accommodated I\'m afraid!'
 
+puts 'I will ask you again one more time... what do you want?!'
+employee_request = gets.chomp
+boss_response = 'whaddaya mean "' + employee_request + '"?!? you\'re fired!!'
+puts (boss_response.upcase)
+
+
