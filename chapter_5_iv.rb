2011d6de5bf3b09439be783b8b19dfa9a5670cb9
ajbtech23 <joshuabanjo.tech@gmail.com>
2024-08-14 16:02:16 +0100

    Almost done here

--- chapter_5_iv.rb
+++ chapter_5_iv.rb
@@ -7,3 +7,16 @@ puts 365%7
 puts ((5-2).abs)
 puts ((2-5).abs)
 
+puts rand 
+puts rand 
+puts rand 
+puts (rand(100))
+puts (rand(100))
+puts (rand(100))
+puts (rand(1))
+puts (rand(1))
+puts (rand(1))
+puts (rand(99999999999999999999))
+
+puts ('The weatherman said there is a ' + rand(101).to_s + '% chance of rain, ')
+puts ('but you can never really trust the weatherman...')
