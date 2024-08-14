a1de2493f0438fb06b6d5a3a3450835985edf657
ajbtech23 <joshuabanjo.tech@gmail.com>
2024-08-14 15:40:30 +0100

    Working through my progressions

--- angry_boss.rb
+++ angry_boss.rb
@@ -1,1 +1,6 @@
 puts 'What do you want Scallywag?'
+employee_request = gets.chomp
+puts 'What was that?! You said you want ' + employee_request.upcase + '? Did I hear you correctly?!'
+employee_request = gets.chomp
+puts 'Can\'t be accommodated I\'m afraid!'
+
