dbe243c3c374c017cb0ff76a60e2f360f3b97d36
ajbtech23 <joshuabanjo.tech@gmail.com>
2024-08-15 13:11:38 +0100

    Some of my best work

--- deaf_grandma_extended.rb
+++ deaf_grandma_extended.rb
@@ -1,13 +1,21 @@
 tawk = ''
 
-while tawk != 'BYE'
+bye_counter = 0 
+
+while bye_counter < 3
+    tawk = gets.chomp
     if tawk == ''
     else 
-        if tawk.upcase == tawk 
-            puts 'NO, NOT SINCE ' + ((1930 + ((rand(21).to_i))).to_s) + '!'
+        if tawk == 'BYE' 
+            puts 'Sorry dear, can you say that again for me please?'
+            bye_counter += 1 
         else 
-            puts 'HUH?! SPEAK UP SONNY JIM!'
+            if tawk.upcase == tawk
+                puts 'NO, NOT SINCE ' + ((1930 + ((rand(21).to_i))).to_s) + '!'
+            else
+                puts 'HUH?! SPEAK UP SONNY JIM!' 
+            end 
+            bye_counter = 0 
         end 
     end 
-    tawk = gets.chomp
 end 
