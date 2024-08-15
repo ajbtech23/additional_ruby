b98e126e52d73d0f8a545a23a79d07723fe4624e
ajbtech23 <joshuabanjo.tech@gmail.com>
2024-08-15 14:58:46 +0100

    Redemption arc

--- chapter_7_ii.rb
+++ chapter_7_ii.rb
@@ -4,3 +4,9 @@ basketball_players.each do |basketball_player|
     puts 'Welcome to the Olympics ' + basketball_player + '! We are so lucky to have you :D'
 end 
 
+languages = ['Python', 'Rust', 'C++', 'Ruby']
+
+languages.each do |language|
+    puts 'One day I will learn how to code effectively in ' + language.upcase + ' and it will be the best thing ever!'
+end 
+
