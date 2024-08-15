a3d7b8d76de1aac8da0db5aada5277407207c5f6
ajbtech23 <joshuabanjo.tech@gmail.com>
2024-08-15 14:54:01 +0100

    This loop is not intuitive and will take time to adjust to

---
+++ chapter_7_ii.rb
@@ -0,0 +1,6 @@
+basketball_players = ['Lebron James', 'Steph Curry', 'Kevin Durant', 'Bam Adebayo', 'Anthony Edwards']
+
+basketball_players.each do |basketball_player|
+    puts 'Welcome to the Olympics ' + basketball_player + '! We are so lucky to have you :D'
+end 
+
