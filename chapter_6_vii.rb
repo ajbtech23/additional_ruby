b6b173b3f4f62a30b91843e438c43366c04e93d7
ajbtech23 <joshuabanjo.tech@gmail.com>
2024-08-15 11:07:55 +0100

    Logical operators and truthys

---
+++ chapter_6_vii.rb
@@ -0,0 +1,12 @@
+iAmChris = true 
+iAmPurple = false 
+iLikeFood = true 
+iEatRocks = false
+
+puts (iAmChris and iLikeFood)
+puts (iLikeFood and iEatRocks)
+puts (iAmPurple and iLikeFood)
+
+puts (not iAmPurple)
+puts (not iAmChris)
+
