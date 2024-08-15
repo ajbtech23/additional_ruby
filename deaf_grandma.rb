f284421eee0f3675c4a335d7323b1caada80deab
ajbtech23 <joshuabanjo.tech@gmail.com>
2024-08-15 12:00:46 +0100

    Overcoming skill atrophy

---
+++ deaf_grandma.rb
@@ -0,0 +1,16 @@
+tawk = ''
+
+while tawk.upcase != 'BYE'
+    #
+    if tawk == ''
+    else
+        if tawk.upcase == tawk
+            puts 'NO, NOT SINCE ' + (((1930 + (rand(21))).to_i).to_s) + '!'
+        else 
+            puts 'HUH?! SPEAK UP, SONNY!'
+        end 
+    end 
+
+    tawk = gets.chomp 
+end 
+
