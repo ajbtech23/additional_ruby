81bcfaee74d9336f3882e71221f4ad43f505ec51
ajbtech23 <joshuabanjo.tech@gmail.com>
2024-08-15 15:14:20 +0100

    Still practicing

---
+++ chapter_7_iv.rb
@@ -0,0 +1,11 @@
+faves = []
+faves.push 'raindrops on roses'
+faves.push 'whiskey on kittens'
+
+puts faves[0]
+puts faves.last 
+puts faves.length 
+
+puts faves.pop 
+puts faves 
+puts faves.length 
