0ece085d0b23483d78f6e2db9b349c1098d1b5e7
ajbtech23 <joshuabanjo.tech@gmail.com>
2024-08-15 14:51:10 +0100

    Arrays and iterators and loops

---
+++ chapter_7_i.rb
@@ -0,0 +1,10 @@
+languages = ['English', 'German', 'Ruby']
+
+languages.each do |lang|
+    puts 'I love ' + lang + '!'
+    puts 'Don\'t you?'
+end 
+
+puts 'And let\'s hear it for C++!'
+puts '...'
+
