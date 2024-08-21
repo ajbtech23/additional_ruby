bd0d21c198780a8d742dda0bf77e6ac40c5e7d20
Joshua Banjo <joshuabanjo.tech@gmail.com>
2024-08-21 01:37:41 +0100

    Completed the conditionals page on The Odin Project

--- conditionals_v.rb
+++ conditionals_v.rb
@@ -5,11 +5,15 @@
 #   puts "Enjoy school!"
 # end
 
-age = 13
-puts "Welcome to a life of debt and misery" unless age < 18
+# age = 13
+# puts "Welcome to a life of debt and misery" unless age < 18
 
-unless age < 18
-  puts "Down with that sort of thing"
-else
-  puts "RICH 4EVA"
-end
+# unless age < 18
+#   puts "Down with that sort of thing"
+# else
+#   puts "RICH 4EVA"
+# end
+
+age = 14
+response = age > 18 ? "Better get to work young man!" : "Enjoy your youth"
+puts response
